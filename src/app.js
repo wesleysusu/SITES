4eaa8a25aae598501d5c09c56bc8225f23abe098
wesleysusu <wesleymdsilva@recife.pe.senac.br>
2022-08-19 16:30:05 -0300

    mensagem

--- src/app.js
+++ src/app.js
@@ -8,14 +8,6 @@ const clientes =[
     {id:2,nome:"macksuel",cpf:"147.548.789-0"}
 ]
 
-const funcionarios =[
-    {id:1,nome:"marcos",cpf:"123.456.789-0"},
-    {id:2,nome:"andre",cpf:"147.548.789-0"}
-]
-
-
-
-
 
 
 
@@ -25,7 +17,7 @@ app.get('/',(req,res) =>{
 })
 //consultar todos os clientes
 
-app.get('/', (req,res) =>{
+app.get('/clientes', (req,res) =>{
     res.status(200).json(clientes)
 })
 
@@ -35,40 +27,33 @@ app.post('/clientes',(req,res) =>{
     res.status(200).send("cliente cadastrado!!!")
 })
 
+//função que realiza a busca por id
 
+function buscarClientes(id){
+    return clientes.findIndex(clientes => clientes.id == id)
+}
 
+//consultar cliente por id
 
+app.get('/clientes/:id',(req,res) =>{
+let index = buscarClientes(req.params.id)
+res.json(clientes[index]);})
 
+app.put('/clientes/:id',(req,res) => {
+    let index = buscarClientes(req.params.id)
+    clientes[index] = req.body
 
+    res.json(clientes)
 
-//rota principal
-app.get('/',(req,res) =>{
-    res.status(200).send("pagina inicial")
-})
-//consultar todos os clientes
-
-app.get('/funcionarios', (req,res) =>{
-    res.status(200).json(funcionarios)
-})
-
-//cadastrar um novo cliente
-app.post('/funcionarios',(req,res) =>{
-    funcionarios.push(req.body)
-    res.status(200).send("funcionario cadastrado!!!")
 })
 
+//excluir cliente por id
+app.delete('/clientes/:id',(req,res) =>{
+    let index = buscarClientes(req.params.id)
+    clientes.splice(index,1)
+    res.send("cliente eliminado")
 
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
@@ -76,3 +61,9 @@ app.post('/funcionarios',(req,res) =>{
 
 
 export default app
+
+
+
+
+
+
